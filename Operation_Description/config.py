e69a5c1f21473e2479870138368953c9607a7a11
RamonJWS <raysantiago1994@gmail.com>
2022-12-26 16:20:45 +0000

    notes and code for tags, response descriptions, status codes, and docstrings

---
+++ Operation_Description/config.py
@@ -0,0 +1,11 @@
+TAGS_METADATA = [
+                    {
+                        "name": "blog",
+                        "description": "operations to do with blogs."
+                    },
+
+                    {
+                        "name": "comment",
+                        "description": "operations to do with comments on blogs"
+                    },
+                ]
